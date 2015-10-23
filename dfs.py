81c1788e8c1d25f8ed1fb81b10b105a218b6218d
김석근 <sgkim@zuminternet.com>
2015-10-23 15:16:42 +0900

    스택을 이용한 dfs 구현

--- dfs.py
+++ dfs.py
@@ -1,20 +1,37 @@
-graph = {'A': ['B', 'C'],
-         'B': ['A', 'D', 'E'],
-         'C': ['A', 'F'],
-         'D': ['B'],
-         'E': ['B', 'F'],
-         'F': ['C', 'E']}
+def dfs(graph, start, stack):
+    stack.append(start)
+    adjacency = graph[start].copy()
+
+    for v in stack:
+        if v in adjacency:
+            adjacency.remove(v)
+    for nxt in adjacency:
+        if nxt not in stack:
+            dfs(graph, nxt, stack)
+    if len(stack) == len(graph.keys()):
+        return stack
+    elif len(stack) > 0:
+        stack.pop()
 
 
-def dfs(graph, start, visited=[]):
-    visited.add(start)
-    print(start)
-    graph[start]
-    for v in visited:
-        pass
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+def word_chain(graph):
+    for key, value in graph.items():
+        result = dfs(graph, key, [])
+        if type(result) is list:
+            return ' '.join(result)
+
+    return "IMPOSSIBLE"
 
 
-print(dfs(graph, 'C'))
+gr = {'A': ['B', 'C'],
+      'B': ['A', 'D', 'E'],
+      'C': ['A', 'F'],
+      'D': ['B'],
+      'E': ['B', 'F'],
+      'F': ['C', 'E']}
+print(word_chain(gr))
+
+
+# dfs(graph, 'A)
+# print(dfs(graph, 'A'))
+# print(dfs(graph, 'B'))
