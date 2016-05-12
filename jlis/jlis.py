89d9d28337ba09c198e50949026fca2d6c721f22
김석근 <sgkim@zuminternet.com>
2016-05-12 16:42:47 +0900

    jlis 제출했지만 시간초과

--- jlis/jlis.py
+++ jlis/jlis.py
@@ -1,44 +1,60 @@
-def get_lis(a):
-    if len(a) == 0:
-        return []
-    lis_len = 0
-    lis = []
-    for i in xrange(len(a)):
-        b = []
-        for j in xrange(i, len(a)):
-            if a[i] < a[j]:
-                b.append(a[j])
-        lis_cand = [a[i]] + get_lis(b)
-        if len(lis_cand) > lis_len:
-            lis = lis_cand
-            lis_len = len(lis)
+import sys
 
-    return lis
+A = []
+B = []
+cache = 101 * [101 * [-1]]
 
 
-def get_inc_seq_list(seq):
-    if len(seq) == 0:
-        return []
-    elif len(seq) == 1:
-        return [[seq[0]]]
+def jlis(idx_a, idx_b):
+    ret = cache[idx_a + 1][idx_b + 1]
+    if ret != -1:
+        return ret
+    ret = 2
+    a = - sys.maxint if idx_a == -1 else A[idx_a]
+    b = - sys.maxint if idx_b == -1 else B[idx_b]
+    max_elem = max(a, b)
+    for i in xrange(idx_a + 1, len(A)):
+        if max_elem < A[i]:
+            ret = max(ret, jlis(i, idx_b) + (1 if idx_a != -1 else 0))
+    for j in xrange(idx_b + 1, len(B)):
+        if max_elem < B[j]:
+            ret = max(ret, jlis(idx_a, j) + (1 if idx_b != -1 else 0))
+    return ret
 
-    is_list = []
-    for i in xrange(len(seq)):
-        is_list += [inc_seq for inc_seq in get_inc_seq_list(seq[i + 1:]) if inc_seq.insert(0, seq[i]) is None]
 
-    return is_list
+def set_A(l):
+    global A
+    A = l
 
 
-def get_jlis_len(a, b):
-    a_list = get_inc_seq_list(a)
-    b_list = get_inc_seq_list(b)
+def set_B(l):
+    global B
+    B = l
 
 
 if __name__ == '__main__':
     c = int(raw_input())
     for i in xrange(c):
         a_len, b_len = (int(inp) for inp in raw_input().split())
-        a = [int(inp) for inp in raw_input().split()]
-        b = [int(inp) for inp in raw_input().split()]
+        set_A([int(inp) for inp in raw_input().split()])
+        set_B([int(inp) for inp in raw_input().split()])
 
-        get_jlis_len(a, b)
+        jlis(0, 0)
+
+
+def lis(a):
+    if len(a) == 0:
+        return []
+    lis_len = 0
+    ret = []
+    for i in xrange(len(a)):
+        b = []
+        for j in xrange(i, len(a)):
+            if a[i] < a[j]:
+                b.append(a[j])
+        lis_cand = [a[i]] + lis(b)
+        if len(lis_cand) > lis_len:
+            ret = lis_cand
+            lis_len = len(ret)
+
+    return ret
--- jlis/test.py
+++ jlis/test.py
@@ -1,17 +1,31 @@
 import unittest
 
-from jlis import get_jlis_len, get_inc_seq_list, get_lis
+from jlis import lis, jlis, set_A, set_B
 
 
 class JilsTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(get_lis([1, 2, 3]), [1, 2, 3])
+        self.assertEqual(lis([1, 2, 3]), [1, 2, 3])
 
     def test2(self):
-        self.assertEqual(get_lis([3, 2, 1, 7, 5, 4, 2, 6]), [3, 5, 6])
+        self.assertEqual(lis([3, 2, 1, 7, 5, 4, 2, 6]), [3, 5, 6])
 
     def test3(self):
-        self.assertEqual(get_inc_seq_list([1, 2, 4]), [[1, 2], [1, 4], [2, 4]])
+        set_A([1])
+        set_B([3])
+        self.assertEqual(jlis(-1, -1), 2)
 
     def test4(self):
-        self.assertEqual(get_jlis_len([1, 2, 4], [3, 4, 7]), 5)
+        set_A([1, 2, 4])
+        set_B([3, 4, 7])
+        self.assertEqual(jlis(-1, -1), 5)
+
+    def test5(self):
+        set_A([1, 2, 3])
+        set_B([4, 5, 6])
+        self.assertEqual(jlis(-1, -1), 6)
+
+    def test6(self):
+        set_A([10, 20, 30, 1, 2])
+        set_B([10, 20, 30])
+        self.assertEqual(jlis(-1, -1), 5)
