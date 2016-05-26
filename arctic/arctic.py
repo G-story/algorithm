1140004b9e54d25940126d89e446822a4a358725
김석근 <sgkim@zuminternet.com>
2016-05-26 15:10:09 +0900

    arctic 시간초과

--- arctic/arctic.py
+++ arctic/arctic.py
@@ -7,18 +7,32 @@ class Arctic:
 
     @classmethod
     def get_min_elec_dist(cls, bases):
-        lines = [[10000 for j in xrange(100)] for i in xrange(100)]
+        lines = []
+        for i, from_xy in enumerate(bases):
+            line = []
+            for j, to_xy in enumerate(bases):
+                line.append(round(math.sqrt((from_xy[0] - to_xy[0]) ** 2 + (from_xy[1] - to_xy[1]) ** 2), 2))
+            lines.append(line)
 
-        for i, xy in enumerate(bases):
-            for j in range(i + 1, len(bases)):
-                lines[i][j] = round(math.sqrt((xy[0] - bases[j][0]) ** 2 + (xy[1] - bases[j][1]) ** 2), 2)
+        selected = len(bases) * [False]
+        selected[0] = True
+        seleted_idxs = [0]
+        selected_lines = []
+        for i in xrange(len(bases) - 1):
+            min_idx, min_dist = 0, 1000
+            for j in seleted_idxs:
+                for k, l in enumerate(lines[j]):
+                    if selected[k] == False and l < min_dist:
+                        min_idx, min_dist = k, l
+            selected[min_idx] = True
+            seleted_idxs.append(min_idx)
+            selected_lines.append(min_dist)
 
-        for ls in lines:
-            pass
+        return max(selected_lines)
 
 
 if __name__ == '__main__':
     c = int(raw_input())
     for i in xrange(c):
         n = int(raw_input())
-        print Arctic.get_min_elec_dist([[float(j) for j in raw_input().split()] for i in n])
+        print Arctic.get_min_elec_dist([[float(j) for j in raw_input().split()] for i in xrange(n)])
